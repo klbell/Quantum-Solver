0f0bf9d2c5208d6430278890c5feef216d5176df
namoore1 <namoore1@ualberta.ca>
2014-06-19 17:24:17 -0600

    Trying to implement the bare bones demonstration w/ ILNumerics in VS. So far I've got the field calculation working (superfast) bu thte visualization is failing. This ILNUmerics package is kind of confusing...

---
+++ FDTDdemo/FDTDdemo/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace FDTDdemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ilPanel1 = new ILNumerics.Drawing.ILPanel();
+            this.SuspendLayout();
+            // 
+            // ilPanel1
+            // 
+            this.ilPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ilPanel1.Driver = ILNumerics.Drawing.RendererTypes.GDI;
+            this.ilPanel1.Editor = null;
+            this.ilPanel1.Location = new System.Drawing.Point(0, 0);
+            this.ilPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ilPanel1.Name = "ilPanel1";
+            this.ilPanel1.Rectangle = ((System.Drawing.RectangleF)(resources.GetObject("ilPanel1.Rectangle")));
+            this.ilPanel1.ShowUIControls = false;
+            this.ilPanel1.Size = new System.Drawing.Size(515, 421);
+            this.ilPanel1.TabIndex = 0;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 421);
+            this.Controls.Add(this.ilPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ILNumerics.Drawing.ILPanel ilPanel1;
+    }
+}
+
---
+++ FDTDdemo/FDTDdemo/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ILNumerics;
+using ILNumerics.Drawing;
+using ILNumerics.Drawing.Plotting;
+using Fields;
+using System.Threading;
+
+namespace FDTDdemo
+{
+    public partial class Form1 : Form
+    {
+        Electric E;
+        Magnetic H;
+        int[] samp = new int[3] { 200, 200, 200 };
+        double[] wind = new double[3] { 200, 200, 200 };
+        int imp0 = 377;
+        public Form1()
+        {
+            InitializeComponent();
+            E = new Electric(3, wind, samp);
+            H = new Magnetic(3, wind, samp);
+
+            int t=0;
+            int m;
+            ILArray<float> A = new ILArray<float>[200];
+            var scene = new ILScene();
+            ilPanel1.Scene = scene;
+            var plotCube = scene.Add(new ILPlotCube());
+            plotCube.Limits.Set(new Vector3(0f,-1f,0f), new Vector3(200f,1f,0f));
+            plotCube.AllowRotation = false;
+            plotCube.AllowZoom = false;
+            plotCube.AllowPan = false;
+            ilPanel1.Scene = scene;
+            for (t = 0; t < 1000; t++)
+            {
+                //calculate fields
+                
+
+                for (m = 0; m < 199; m++)
+                    H.Value[0][m, 0, 0] = H.Value[0][m, 0, 0] + (E.Value[0][m + 1, 0, 0] - E.Value[0][m, 0, 0]) / imp0;
+                for (m = 1; m < 200; m++)
+                    E.Value[0][m, 0, 0] = E.Value[0][m, 0, 0] + (H.Value[0][m, 0, 0] - H.Value[0][m - 1, 0, 0]) * imp0;
+                E.Value[0][0, 0, 0] = Math.Exp(-(Math.Pow((t - 30f),  2f)) / 100f);
+                if (t % 10 == 0)
+                {
+                    for (m = 0; m < 200; m++)
+                        A[m] = Convert.ToSingle(E.Value[0][m, 0, 0]);
+                    plotCube.Reset();
+                    plotCube.Add(new ILLinePlot(A.T));
+
+                    //I can't get the plot to update.. :(
+
+                }
+
+                Application.DoEvents();
+            }
+        }
+
+    }
+}
